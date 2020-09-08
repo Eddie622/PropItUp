abb3ec6fddd0a252e171319a76f6b606996d03df
heriberto.torres <eddietorres622@gmail.com>
2020-09-08 15:05:51 -0400

    added state function

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Person from './components/Person';
 function App() {
   return (
     <div className="App">
-      <Person firstname="Jane" lastname="Doe" age="45" hair="Black"/>
-      <Person firstname="John" lastname="Smith" age="88" hair="Brown"/>
-      <Person firstname="Millard" lastname="Fillmore" age="50" hair="Brown"/>
-      <Person firstname="Maria" lastname="Smith" age="62" hair="Brown"/>
+      <Person firstname="Jane" lastname="Doe" age={45} hair="Black"/>
+      <Person firstname="John" lastname="Smith" age={88} hair="Brown"/>
+      <Person firstname="Millard" lastname="Fillmore" age={50} hair="Brown"/>
+      <Person firstname="Maria" lastname="Smith" age={62} hair="Brown"/>
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age : this.props.age
+        };
+    }
+
+    age = (e) => {
+        this.setState({age : this.state.age + 1});
+    }
+
     render() {
         return (
             <div>
                 <h1 className="person-name">{this.props.lastname}, {this.props.firstname}</h1>
-                    <p className="person-age">Age: {this.props.age}</p>
+                    <p className="person-age">Age: {this.state.age}</p>
+                    <button onClick = {this.age}>Birthday Button</button>
                     <p className="person-hair-color">Hair Color: {this.props.hair}</p>
             </div>
         );
